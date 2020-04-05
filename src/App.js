6ae1d21bbf8e2496374ca9fc47f10b1a7511c27e
sumit <sandeep@triadsys.com>
2020-04-05 03:53:47 -0400

    added  redirect , redux

--- src/App.js
+++ src/App.js
@@ -1,42 +1,40 @@
-import React from 'react';
-import { Switch ,Route } from 'react-router-dom';
+import React, { useReducer } from 'react';
+import { Switch ,Route , Redirect } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 
+import { connect } from 'react-redux';
 import Header from './Components/header/header.component';
 import SignInAndSignUp from './pages/Sign-in-and-Sign-out/Sign-in-and-Sign-out.component';
+
+import { setCurrentUser }  from './redux/user/user.action'
 
 import { auth  , createUserProfileDocument } from './firebase/firebase.utils';
 
 
 class App extends React.Component {
-constructor(){
-  super();
 
-  this.state = {
-    currentUser : null
-  };
-}
 
 unsubscribeFromAuth = null
 
 componentDidMount(){
+  const { setCurrentUser } = this.props;
   this.unsubscribeFromAuth=auth.onAuthStateChanged(async userAuth =>{
     if(userAuth) {
       const userRef = await createUserProfileDocument(userAuth);
 
       userRef.onSnapshot(snapShot =>{
-       this.setState({
-         currentUser:{
+      setCurrentUser({
+         
            id:snapShot.id,
            ...snapShot.data()
-         }
+         
        });
-       console.log(this.state);
+       
       });
     }
-     this.setState({ currentUser: userAuth })
+       setCurrentUser(userAuth)
     
   });
 
@@ -50,12 +48,12 @@ componentWillUnmount(){
     return(
       <div>
         
-        <Header currentUser={this.state.currentUser} />
-        <switch>
+        <Header />
+        <Switch>
           <Route exact path = "/" component={HomePage} />
           <Route path ="/Shop" component={ShopPage} />
-          <Route path ="/signin" component={SignInAndSignUp} />
-        </switch>
+          <Route exact path ="/signin" render={()=>this.props.currentUser ? (<Redirect to='/' />):(<SignInAndSignUp />)} />
+        </Switch>
       </div>
   
     );
@@ -65,5 +63,11 @@ componentWillUnmount(){
  
     
 }
+const mapStateToProps =({ user })=>({
+  currentUser : user.currentUser
+})
+const mapDispatchToProps = dispatch =>({
+  setCurrentUser: user => dispatch(setCurrentUser(user)) //pass gonna be action
+})
 
-export default App;
+export default connect (mapStateToProps,mapDispatchToProps )(App);
--- src/Components/header/header.component.jsx
+++ src/Components/header/header.component.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+ 
+import { connect } from 'react-redux';
+ 
 import  { ReactComponent as Logo }  from '../../assets/crown.svg'
+
+
 import { auth } from '../../firebase/firebase.utils';
+
+
+
 import './header.style.scss';
 
 const Header = ({ currentUser }) =>(
@@ -31,4 +39,9 @@ const Header = ({ currentUser }) =>(
     </div>
 );
 
-export default Header;
+const mapStateToProps = state =>({
+currentUser: state.user.currentUser
+
+});
+
+export default connect(mapStateToProps) (Header);
